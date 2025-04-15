2aa552955684cfc437ba0ba1f4b4ffaecfa12398
Julio Anccasi <julio.anccasi.dev@gmail.com>
2025-04-15 15:43:29 -0500

    fix: agregar puerto dinámico para Railway

--- main.py
+++ main.py
@@ -17,3 +17,10 @@ def get_db():
 @app.get("/productos")
 def listar_productos(db: Session = Depends(get_db)):
     return db.query(Producto).all()
+
+import os
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
